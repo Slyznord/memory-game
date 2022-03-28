73d6f266cc96b7f006f364302d86fb2d75fd927c
Maikl <38893957+Slyznord@users.noreply.github.com>
2022-03-28 16:02:20 +0300

    Delete src directory

--- src/API/api.config.ts
+++
@@ -1,1 +0,0 @@
-export const baseURL = process.env.NODE_ENV === 'production' ? '/memory-game/' : '/'
--- src/components/Grid.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="grid">
-    <slot name="content"></slot>
-  </div>
-</template>
